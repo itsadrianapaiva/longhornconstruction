20874f6bb8d0ea26e8e8d397d032b1b2f4717dfb
Adriana Paiva <itsadrianapaiva@gmail.com>
2026-07-17 00:47:59 +0100

    updated fb url

--- lib/content/longhornSocial.ts
+++ lib/content/longhornSocial.ts
@@ -1,13 +1,9 @@
-/**
- * TODO: add facebook 
- */
-
 export type SocialLinks = {
   facebook: string;
   instagram: string;
 };
 
 export const SOCIAL_LINKS: SocialLinks = {
-  facebook: "https://www.facebook.com/",
+  facebook: "https://www.facebook.com/longhornconstructionlda",
   instagram: "https://www.instagram.com/longhornconstructionlda",
 };
